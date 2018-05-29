615627b3beeeea69085aaf83fc0cccd0a99ba7d2
hellojdh <jdh940130@gmail.com>
2018-05-29 19:09:57 +0900

    5/29 Commit

---
+++ src/baekjoon/q1000/S1001.java
@@ -0,0 +1,11 @@
+package baekjoon.q1000;
+
+import java.util.Scanner;
+
+public class S1001 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() - sc.nextInt());
+		sc.close();
+	}
+}
