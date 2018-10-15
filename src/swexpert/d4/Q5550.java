71ae5954a70ec7a06cf57dad538fe5ad7b31d60b
jdh <jdh940130@gmail.com>
2018-10-15 16:35:02 +0900

    5550. 나는 개구리로소이다(리팩토링 check()추가)

--- src/swexpert/d4/Q5550.java
+++ src/swexpert/d4/Q5550.java
@@ -13,7 +13,7 @@ public class Q5550 {
 			String t = br.readLine();
 			int result = 0; // 개구리 수
 			int tResult = 0; // 잉여 개구리 수
-			boolean flag = true;
+			flag = true;
 			arr = new int[5]; // c r o a k
 			// 각 문자마다 +1을 시켜주고 다음번 문자에서는 전 문자를 -1 시켜준다.
 			// k 까지 잘 도착시 tResult(잉여 개구리수)를 +1 시켜준다.
@@ -30,31 +30,19 @@ public class Q5550 {
 					break;
 				case 'r':
 					arr[0]--; arr[1]++;
-					if(arr[0]<0) {
-						flag = false;
-						result = -1;
-					}
+					if(check(0)) result = -1;
 					break;
 				case 'o':
 					arr[1]--; arr[2]++;
-					if(arr[1]<0) {
-						flag = false;
-						result = -1;
-					}
+					if(check(1)) result = -1;
 					break;
 				case 'a':
 					arr[2]--; arr[3]++;
-					if(arr[2]<0) {
-						flag = false;
-						result = -1;
-					}
+					if(check(2)) result = -1;
 					break;
 				case 'k':
 					arr[3]--; tResult++;
-					if(arr[3]<0) {
-						flag = false;
-						result = -1;
-					}
+					if(check(3)) result = -1;
 					break;
 				}
 				if(!flag) break;
@@ -68,5 +56,14 @@ public class Q5550 {
 			}
 			System.out.println("#"+i+" "+result);
 		}
+	}
+	
+	static boolean flag;
+	private static boolean check(int i) {
+		if(arr[i]<0) {
+			flag = false;
+			return true;
+		}
+		else return false;
 	}
 }
