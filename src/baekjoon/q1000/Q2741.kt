e6f4853f188d295d3571962d40f628157c284e11
jdh <jdh940130@gmail.com>
2018-11-05 18:54:43 +0900

    2741. N 찍기

---
+++ src/baekjoon/q1000/Q2741.kt
@@ -0,0 +1,10 @@
+package baekjoon.q1000
+
+import java.util.*
+
+fun main(args:Array<String>){
+    var sc : Scanner = Scanner(System.`in`)
+    var a = sc.nextInt()
+    for (i in 1..a step 1)
+        println(i)
+}
