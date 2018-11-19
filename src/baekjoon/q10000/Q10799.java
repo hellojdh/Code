e9e956b4c68e52285497ff221057b654328d5768
jdh <jdh940130@gmail.com>
2018-11-19 23:14:58 +0900

    10799. 쇠막대기

--- src/baekjoon/q10000/Q10799.java
+++ src/baekjoon/q10000/Q10799.java
@@ -1,25 +1,26 @@
 package baekjoon.q10000;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Q10799 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        Stack stack = new Stack();
         String t = sc.next();
-        int cnt = 0;
         int result = 0;
         for(int i=0;i<t.length();i++){
             if(t.charAt(i)=='('){
                 if(t.charAt(i+1)==')'){
                     i++;
-                    result+=cnt;
+                    result += stack.size();
                     continue;
                 }
                 // 막대기 개수 +1
-                cnt++;
+                stack.push(1);
             }else{
                 // 막대기 개수 -1
-                cnt--;
+                stack.pop();
                 // 총 막대 개수 +1
                 result++;
             }
