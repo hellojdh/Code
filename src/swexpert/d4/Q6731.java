bc179afca2a3e4a7ed8dd757d107885b59fab5e6
jdh <jdh940130@gmail.com>
2019-01-14 21:34:36 +0900

    6731. 홍익이의 오델로 게임

--- src/swexpert/d4/Q6731.java
+++ src/swexpert/d4/Q6731.java
@@ -19,7 +19,7 @@ public class Q6731 {
             // W = false, B = true
             for(int i=0;i<n;i++){
                 String t = br.readLine();
-                // 입력 하면서 가로 탐색
+                // 입력 하면서 가로 세로 개수 세기
                 for(int j=0;j<n;j++) {
                     if (t.charAt(j) == 'B') {
                         arr[i][j] = true;
@@ -30,11 +30,11 @@ public class Q6731 {
             }
 
             int result = 0;
-            // 세로 개수 세기
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
                     int t = garo[i]+sero[j];
                     if(arr[i][j]){
+                        // 검은색일 경우 -1해주기
                         if(--t%2!=0) result++;
                     }else{
                         if(t%2!=0) result++;
